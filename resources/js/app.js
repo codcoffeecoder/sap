5b745ca980fad11f9bd2691637150d0e8f048d84
Your Name <you@example.com>
2021-04-29 18:34:44 +0800

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,30 @@
+require("./bootstrap");
+
+require('bootstrap-select');
+
+window.moment = require('moment');
+window.daterangepicker = require('bootstrap-daterangepicker');
+moment().format();
+
+import swal from 'sweetalert2';
+const Swal = window.Swal = swal;
+const Toast = window.Toast = Swal.mixin({
+  toast: true,
+  position: 'bottom-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  onOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+});
+
+// const CodeMirror = window.CodeMirror = require('codemirror/src/codemirror');
+const SimpleMDE = window.SimpleMDE = require('simplemde/dist/simplemde.min');
+require('summernote/dist/summernote-bs4');
+require('gijgo/js/gijgo');
+require('bootstrap-table');
+require('bootstrap-table/dist/extensions/resizable/bootstrap-table-resizable');
+require('bootstrap-table/dist/extensions/sticky-header/bootstrap-table-sticky-header');
+require('bootstrap-table/dist/extensions/reorder-rows/bootstrap-table-reorder-rows');
---
+++ webpack.mix.js
@@ -0,0 +1,38 @@
+const mix = require("laravel-mix");
+
+mix.js("resources/js/app.js", "public/js")
+    .scripts([
+        'resources/js/jCookie.js',
+        'resources/js/sb-admin-2.js',
+        'resources/js/jquery.easing.js'
+    ], 'public/js/all.js')
+    .scripts([
+        'resources/js/datatableformhandling.js'
+    ], 'public/js/datatableformhandling.min.js')
+    .styles([
+        'resources/sass/sb-admin-2.css'
+    ], 'public/css/all.css')
+    .sass(
+        "resources/sass/app.scss",
+        "public/css"
+    ).extract([
+        'jquery',
+        'axios',
+        'lodash',
+        'moment',
+        'popper.js'
+    ])
+    .extract([
+        'bootstrap',
+        'bootstrap-daterangepicker',
+        'bootstrap-select',
+        'bootstrap-table',
+        'daterangepicker',
+        'gijgo',
+        'sweetalert2'
+    ],'public/bootstrap/bootstrap-lib')
+    .extract([
+        'codemirror',
+        'simplemde',
+        'summernote'
+    ],'public/bootstrap/editor-lib');
